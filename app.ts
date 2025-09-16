72ac32693e2b6914a7a20d9c3653b617a424a30e
Nikhil Shrivastava <nikhil.shrivastava304@gmail.com>
2025-09-16 21:15:36 +0530

    day 5: progress update - Learned about parameter properties in TypeScript - Explored the use of getters and setters - Worked with static members in classes - Understood abstract classes and how they are used for inheritance

--- app.ts
+++ app.ts
@@ -162,3 +162,66 @@ class UserDets {
 
 let u1 = new UserDets("Nikhil");
 u1.changeName();
+
+//@Classes and Objects: Parameter Properties
+class UserData {
+  /*Parameter Properties: By prefixing a constructor parameter with an access modifier ( public, private, protected ) or the readonly modifier, TypeScript automatically performs two actions:
+  - Declares a class property: with the same name and type as the constructor parameter.
+  - Initializes that class property: with the value passed to the corresponding constructor parameter.
+  */
+  constructor(
+    public name: string,
+    public age: number,
+    public gender?: string //This becomes optional
+  ) {}
+}
+
+let ud1 = new UserData("Nikhil", 20, "Male");
+let ud2 = new UserData("Alex", 18);
+
+//@Getters and Setters in TypeScript
+class People {
+  constructor(public _name: string, public age: number) {}
+  get name() {
+    return this._name;
+  }
+  set name(value: string) {
+    this._name = value;
+  }
+}
+
+let p1 = new People("Nikhil", 20);
+console.log(p1._name);
+p1.name = "Dhanashree";
+
+//@Classes and Objects: Static Members
+class Shery {
+  //static keywords makes variables or properties accessible without making a class instance of the class
+  //Means no use of new keyword to make an object, directly you can access it's variables and properties (methods)
+  static version: 2.1;
+
+  static getRandomNumber() {
+    return Math.random();
+  }
+}
+
+// let sh1 = new Shery();   --No use of this when static initialsed and if initialsed, it doesn't get added to Object
+
+//@Classes and Objects: Abstract Classes
+class Payments {
+  constructor(protected amount: number, protected account_no: number) {
+    this.amount = this.validateAmount(amount);
+  }
+
+  private validateAmount(amount: number): number {
+    if (amount > 0) {
+      return amount;
+    } else {
+      throw new Error("Amount must be greater than 0.");
+    }
+  }
+}
+
+//Abstract classes do not require instances to be created; they are used solely for inheritance and extending in other classes. They act as base classes, containing generic functionality. To make them more specific, they can be extended, and specific functionality can be added in the derived classes.
+
+class Paytm extends Payments {}
